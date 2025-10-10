211c0aa81aafbd9e6b98d7833be5c1a35ec1831f
Arr0wing <3650138528@qq.com>
2025-10-10 15:41:41 +0800

    Upload Work1

---
+++ 陈明坤-42312203/第一次作业/Work1/app/src/main/java/tk/growuphappily/work1/MainActivity.java
@@ -0,0 +1,57 @@
+package tk.growuphappily.work1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        findViewById(R.id.explicit).setOnClickListener((view) -> {
+            Intent intent = new Intent(view.getContext(), SecondActivity.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.forResult).setOnClickListener((view) -> {
+            Intent intent = new Intent(view.getContext(), ThirdActivity.class);
+            startActivityForResult(intent, ThirdActivity.REQ_CODE);
+        });
+        findViewById(R.id.forResult).setOnLongClickListener((view) -> {
+            Toast.makeText(getApplicationContext(), "长按了带返回结果的跳转", Toast.LENGTH_LONG).show();
+            return true;
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == ThirdActivity.REQ_CODE)
+        {
+            if(resultCode == ThirdActivity.RESULT_SUCCEED)
+            {
+                TextView textView = findViewById(R.id.text);
+                textView.setText(data.getStringExtra("Ret"));
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(getApplicationContext(), "用户取消操作", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ 陈明坤-42312203/第一次作业/Work1/app/src/main/java/tk/growuphappily/work1/ThirdActivity.java
@@ -0,0 +1,39 @@
+package tk.growuphappily.work1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ThirdActivity extends AppCompatActivity {
+    public static final int RESULT_SUCCEED = 102;
+    public static final int REQ_CODE = 101;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        findViewById(R.id.ret).setOnClickListener((view) -> {
+            Intent intent = new Intent();
+            intent.putExtra("Ret", "1234");
+            setResult(RESULT_SUCCEED, intent);
+            finish();
+        });
+        findViewById(R.id.cancel).setOnClickListener((view) -> {
+            setResult(Activity.RESULT_CANCELED);
+            finish();
+        });
+    }
+}
