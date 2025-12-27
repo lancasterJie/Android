24f2753446080ed7c04c310943a501c65193f962
WeiLongbao2005 <3312334500@qq.com>
2025-12-27 14:12:35 +0800

    Create Record.java

---
+++ 第四次作业/java/Record.java
@@ -0,0 +1,56 @@
+package com.example.myproject4;
+
+public class Record {
+    private int id;
+    private String title;
+    private String content;
+    private String time;
+
+    public Record()
+    {
+        this.id=id;
+        this.title=title;
+        this.content=content;
+        this.time=time;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id=id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title=title;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content=content;
+    }
+
+    public String getTime()
+    {
+        return time;
+    }
+
+    public void setTime(String time)
+    {
+        this.time=time;
+    }
+}
